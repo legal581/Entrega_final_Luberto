f656a155ecfe6d38c45c255a54466f32c41a759c
legal581 <140763685+legal581@users.noreply.github.com>
2023-11-16 17:11:47 -0300

    Localstorage

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,21 +1,21 @@
 //Declaración de variables y arrays//
 const siglas = ['AB', 'BC', 'CD', 'EF'];
 const formasfarma = ['COR', 'COM', 'EMU', 'CPR']
-let Analista = prompt("Ingrese su sigla de Analista");
+let analista = prompt("Ingrese su sigla de Analista");
 
 // Condiciones para datos válidos para Analista
-while (Analista != String) {
-    if (siglas.includes(Analista.toUpperCase())) {
-        console.log(Analista);
+while (analista != String) {
+    if (siglas.includes(analista.toUpperCase())) {
+        console.log(analista);
         break;
     } else {alert("Por favor, ingrese una sigla de Analista válida");}
-    Analista = prompt("Ingrese su sigla de Analista");
+    analista = prompt("Ingrese su sigla de Analista");
 }
 // Condiciones de datos válidos para moléculas
 const productos = ['DICLO', 'IBU', 'PARA', 'OME']
-let Molecula = prompt("Ingrese un producto del siguiente listado: Diclo - Ibu - Para - Ome");
-let i = Molecula;
-while (Molecula !=String) {
+let molecula = prompt("Ingrese un producto del siguiente listado: Diclo - Ibu - Para - Ome");
+let i = molecula;
+while (molecula !=String) {
     if (productos.includes(i.toUpperCase())) {
        console.log(i);
        break;
@@ -24,9 +24,9 @@ while (Molecula !=String) {
 }
 
 // Condiciones de datos válidos para la dosis en números
-let Dosis = parseFloat(prompt("Dosis en mg"));
-let d = Dosis
-while (Dosis != "ESC") {
+let dosis = parseFloat(prompt("Dosis en mg"));
+let d = dosis
+while (dosis != "ESC") {
     if (Number(d)==d && d > 0) {
        console.log(d);
        break;
@@ -35,9 +35,9 @@ while (Dosis != "ESC") {
 }
 
 // Condiciones de datos válidos para Formas Farmacéuticas
-let FormaF = prompt("Forma Farmacéutica");
-let f = FormaF;
-while (FormaF != String) {
+let formaF = prompt("Forma Farmacéutica");
+let f = formaF;
+while (formaF != String) {
     if (formasfarma.includes(f.toUpperCase())) {
         console.log(f);
         break;
@@ -45,25 +45,42 @@ while (FormaF != String) {
     f = prompt("Forma Farmacéutica");
 }
 
-console.log(siglas.indexOf(Analista.toUpperCase()));
-console.log(productos.indexOf(Molecula.toUpperCase()));
-console.log(formasfarma.indexOf(FormaF.toUpperCase()));
+console.log(siglas.indexOf(analista.toUpperCase()));
+console.log(productos.indexOf(molecula.toUpperCase()));
+console.log(formasfarma.indexOf(formaF.toUpperCase()));
 
 //Funciones para generar el código//
 function retornarstring(){
-    return Analista + Molecula + FormaF;}
+    return analista + molecula + formaF;}
 let devuelvevalor = retornarstring();
 console.log(devuelvevalor);
 
 function generarcodigo(){
-    return siglas.indexOf(Analista.toUpperCase()) + '2023' + productos.indexOf(i.toUpperCase()) + formasfarma.indexOf(f.toUpperCase());
+    return siglas.indexOf(analista.toUpperCase()) + '2023' + productos.indexOf(i.toUpperCase()) + formasfarma.indexOf(f.toUpperCase()) + "-" + dosis;
 }
 let codigosigla = generarcodigo();
 console.log(codigosigla);
 alert('El código de su producto es el' + ' ' + codigosigla)
 
 
+//Función para guardar en localstorage y cargar el contenido
+function guardar_localstorage() {
 
+let producto_definido = {
+    nombre: analista,
+    proyecto: molecula,
+    mg: dosis,
+    form: formaF
+};
+let proyecto = codigosigla;
+
+    localStorage.setItem(proyecto, JSON.stringify(producto_definido));
+    let nuevo_producto = JSON.parse(localStorage.getItem(proyecto));
+    console.log(nuevo_producto);
+    
+}
+
+guardar_localstorage();
 
 
 
